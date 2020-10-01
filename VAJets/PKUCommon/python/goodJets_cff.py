dd41fa0a978f4bf018c6e0c31ac2e5f78b19e54c
Ying An <ying.an@cern.ch>
2020-10-01 14:45:24 +0200

    update for PUJetID

--- VAJets/PKUCommon/python/goodJets_cff.py
+++ VAJets/PKUCommon/python/goodJets_cff.py
@@ -1,28 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
 
-goodAK4Jets = cms.EDFilter("PFJetIDSelectionFunctorFilter",
-                        filterParams = pfJetIDSelector.clone(),
-                        src = cms.InputTag("slimmedJets"),
-			filter = cms.bool(True)
-                        )
-from RecoJets.JetProducers.PileupJetID_cfi import _chsalgos_94x
-process.load("RecoJets.JetProducers.PileupJetID_cfi")
-pileupJetIdUpdated = pileupJetId.clone(
-                jets=cms.InputTag("goodAK4Jets"),
-                inputIsCorrected=True,
-                applyJec=False,
-                vertexes=cms.InputTag("offlineSlimmedPrimaryVertices"),
-                algos = cms.VPSet(_chsalgos_94x)
-           )
-### Cleaning
+#goodAK4Jets = cms.EDFilter("PFJetIDSelectionFunctorFilter",
+#                        filterParams = pfJetIDSelector.clone(),
+#                        src = cms.InputTag("slimmedJets"),
+#			filter = cms.bool(True)
+#                        )
+#
+
+
+
+#### Cleaning
 # We want to make sure that the jets are not the electrons or muons done previously
 
 import PhysicsTools.PatAlgos.cleaningLayer1.jetCleaner_cfi as jetCleaner_cfi
 
 cleanAK4Jets = jetCleaner_cfi.cleanPatJets.clone()
 cleanAK4Jets.src = "goodAK4Jets"
-#cleanAK4Jets.src = "pileupJetIdUpdated"
 cleanAK4Jets.checkOverlaps.muons.src = "goodMuons"
 cleanAK4Jets.checkOverlaps.muons.deltaR = 0.4
 cleanAK4Jets.checkOverlaps.muons.requireNoOverlaps = True
@@ -37,5 +31,5 @@ cleanAK4Jets.finalCut = "pt > 20 & abs(eta) < 4.7"
 
 
 
-#NJetsSequence = cms.Sequence(goodAK4Jets+pileupJetIdUpdated + cleanAK4Jets )
-NJetsSequence = cms.Sequence(goodAK4Jets+ cleanAK4Jets )
+#NJetsSequence = cms.Sequence(goodAK4Jets+ cleanAK4Jets )
+NJetsSequence = cms.Sequence( cleanAK4Jets )
